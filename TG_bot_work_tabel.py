abf794fa5e1ace041eb6fa21f3e5d268889aaa72
asgardecz <vanonvag@gmail.com>
2025-01-24 21:38:13 +0400

    Add files via upload

--- TG_bot_work_tabel.py
+++ TG_bot_work_tabel.py
@@ -1,30 +1,47 @@
 from datetime import datetime
 class Company:
-    def __init__(self, name_company):
-        self.name_company = name_company
-        self.notes = [] # Список работ, связанных с этой компанией
+    def __init__(self):
+        self.notes = [] # Общий список выполненных работ для конкретной компании
 
-
-class Note:
-    def __init__(self,company_name,  place, work):
+class Parametr:
+    def __init__(self, place, work): #что я делал
         self.date = datetime.now()  # Дата создания записи
-        self.company_name = company_name  # Название компании
         self.place = place  # Место работы
         self.work = work           # Описание выполненных работ
 
 class NoteManager:
     def __init__(self):
         self.companys = {}  # словарь компаний
-        self.notes = []  #Общий стек всех записей(список объектов Note
 
     def add_company(self, name_company):
         if name_company in self.companys:
             raise ValueError("Компания с таким названием уже существует.")
-        self.companys[name_company] = Company(name_company)
+        self.companys[name_company] = Company()
 
     def add_work(self, name_company, place, work):
-        task = Note(place, work)
+        task = Parametr(place, work)
         self.companys[name_company].notes.append(task)
+
+    def get_notes(self, name_company):
+        if name_company not in self.companys:
+            raise ValueError(f"Компания '{name_company}' не найдена.")
+        return self.companys[name_company].notes
+
+    def get_full_notes_for_company(self, company_name):
+        if company_name not in self.companys:
+            raise ValueError(f"Компания '{name_company}' не найдена.")
+        return self.companys[company_name].notes
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -33,7 +50,7 @@ name_company = input('Введите название компании - ')
 manager.add_company(name_company)
 place = input('Место работы - ')
 work = input('Что было сделано - ')
-manager.add_work(name_company, place,work)
+manager.add_work(name_company, place, work)
 
 print('Ваш табель:')
 for company_name, company in manager.companys.items():
