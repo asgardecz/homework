ef5f84639fb94b34d00fc77a46ca1b1a79061711
asgardecz <vanonvag@gmail.com>
2025-01-25 19:17:56 +0400

    Add files via upload

--- TG_bot_work_tabel.py
+++ TG_bot_work_tabel.py
@@ -22,7 +22,7 @@ class NoteManager:
         task = Parametr(place, work)
         self.companys[name_company].notes.append(task)
 
-    def get_notes(self, name_company):
+    def get_notes_from_date(self, name_company):
         if name_company not in self.companys:
             raise ValueError(f"Компания '{name_company}' не найдена.")
         return self.companys[name_company].notes
@@ -30,10 +30,17 @@ class NoteManager:
     def get_full_notes_for_company(self, company_name):
         if company_name not in self.companys:
             raise ValueError(f"Компания '{name_company}' не найдена.")
-        return self.companys[company_name].notes
+        else:
+            company = manager.companys[company_name]  # получаем объект Company
+            for note in company.notes: # перебираем работы компании
+                formatted_date = note.date.strftime("%d.%m")
+                print(f"{note.date} - {note.place} - {note.work}")
 
 
-
+    def get_all_companies(self):
+            if not self.companys:
+                return "Нет добавленных компаний."
+            return list(self.companys.keys())
 
 
 
@@ -50,11 +57,12 @@ name_company = input('Введите название компании - ')
 manager.add_company(name_company)
 place = input('Место работы - ')
 work = input('Что было сделано - ')
-manager.add_work(name_company, place, work)
+manager.add_work(name_company, place, work)      #всегда ссылайся сначала на класс, затем на функцию
+manager.get_full_notes_for_company(name_company)
 
-print('Ваш табель:')
-for company_name, company in manager.companys.items():
-    print(f"Компания: {company_name}")
-    for note in company.notes:
-        formatted_date = note.date.strftime("%d.%m")
-        print(formatted_date,' - ', note.place,' - ', note.work)
+#print('Ваш табель:')
+#for company_name, company in manager.companys.items():
+#    print(f"Компания: {company_name}")
+#    for note in company.notes:
+#        formatted_date = note.date.strftime("%d.%m")
+#        print(formatted_date,' - ', note.place,' - ', note.work)
